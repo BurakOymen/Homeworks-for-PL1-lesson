4f14fd294e8e5d6e52a7d20a0c84bd23bc94fd63
BurakOymen <93653409+BurakOymen@users.noreply.github.com>
2022-12-07 18:51:00 +0300

    Add files via upload

---
+++ homework1_question7.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  
+  /* Print the square of all numbers from 1 to 10. */
+  
+  int i;
+  
+  for(i=1 ; i<=10 ; i++)
+  {
+          printf("%d\n", i*i);
+  }        
+
+  system("PAUSE");
+  	
+  return 0;
+  
+}
