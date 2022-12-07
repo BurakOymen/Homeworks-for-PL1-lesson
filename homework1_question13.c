3b5460a908705f7d0254db8880ee0a8f08f44379
BurakOymen <93653409+BurakOymen@users.noreply.github.com>
2022-12-08 00:51:25 +0300

    Add files via upload

---
+++ homework1_question13.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  
+  /* Print the multiplication table for 6's. */
+  
+  int k;
+  
+  for(k=1 ; k<=10 ; k++)
+  {
+          printf("6 * %d = %d\n", k, 6*k);
+  }         
+    
+  system("PAUSE");
+  	
+  return 0;
+  
+}
