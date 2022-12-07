bfd5121e0b3ab44e3f31c53b68eca7fed38f6b42
BurakOymen <93653409+BurakOymen@users.noreply.github.com>
2022-12-07 18:02:08 +0300

    Add files via upload

---
+++ homework1_question1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  
+  /* Add two numbers entered by user */
+  
+  int a, b;
+  
+  printf("Please enter two numbers\n");
+  
+  scanf("%d%d", &a, &b);
+  
+  printf("Sum of these numbers is : %d\n", a+b);  
+  
+  system("PAUSE");
+	
+  return 0;
+
+}
