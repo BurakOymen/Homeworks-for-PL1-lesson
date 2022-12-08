556439c311568e95d848ca7c7270141c7da708b5
BurakOymen <93653409+BurakOymen@users.noreply.github.com>
2022-12-08 22:38:18 +0300

    Add files via upload

---
+++ homework1_question19.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  
+  /* Print multiplication table of any number. */
+  
+  int n, j;
+  
+  printf("Please enter a number to print its multiplication number\n");
+  
+  scanf("%d", &n);
+  
+  printf("\n");
+  
+  for(j=1 ; j<=10 ; j++)
+  {
+          printf("%d * %d = %d\n", n, j, n*j);
+  }     
+   
+  system("PAUSE");
+  	
+  return 0;
+  
+}
