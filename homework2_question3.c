e4d0f6bf0cc9a8977e63b0803dd0ed31fdc3abf3
Burak Öymen <93653409+BurakOymen@users.noreply.github.com>
2022-12-23 22:59:17 +0300

    Add files via upload

---
+++ homework2_question3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  
+  /* Print Hello World 10 times. */
+  
+  int i;
+  
+  for(i=1 ; i<=10 ; i++)
+  {
+       printf("Hello World\n"); 
+  }        
+  
+  system("PAUSE");	
+  
+  return 0;
+  
+}
