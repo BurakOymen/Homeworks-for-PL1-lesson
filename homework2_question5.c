ae3de007e49f39c5b80bac3befd20519eaf22a58
Burak Öymen <93653409+BurakOymen@users.noreply.github.com>
2022-12-23 23:00:09 +0300

    Add files via upload

---
+++ homework2_question5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+  
+  /* C program to calculate 2^4 */
+  
+  int r;
+  
+  r = pow(2, 4);
+  
+  printf("Result of 2^4 is : %d\n", r); 
+   
+  system("PAUSE");
+  	
+  return 0;
+  
+}
